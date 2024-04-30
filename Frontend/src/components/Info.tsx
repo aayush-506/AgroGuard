a28732eaee67d599f9ad90e6cfd61f3c0bf0a0a0
Sahas Timilsina <tsahas2059@gmail.com>
2024-04-30 14:58:53 +0545

    test push

--- Frontend/src/components/Info.tsx
+++ Frontend/src/components/Info.tsx
@@ -4,7 +4,7 @@ export function Info() {
   return (
     <div className="w-1/2 p-4 border-gray-400 flex flex-col justify-center item-center">
       <div className="p-4 flex w-full h-full justify-center items-center border-b border-gray-400">
-        Disease Name Lorem ipsum dolor sit amet, qui minim labore adipisicing
+        Disease Name: Lorem ipsum dolor sit amet, qui minim labore adipisicing
         minim sint cillum sint consectetur cupidatat.
       </div>
       <div className="p-4 w-full flex justify-center items-center">
